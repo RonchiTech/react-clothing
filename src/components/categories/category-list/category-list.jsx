8d8d1194ea2dc2f30407f22263cebbfe1f59e2cf
ronchitech <ronchibusiness@gmail.com>
2022-05-15 15:52:16 +0800

    add components and stylings for clothing

---
+++ src/components/categories/category-list/category-list.jsx
@@ -0,0 +1,14 @@
+import CategoryItem from '../category-item/category-item.component';
+import './category-list.styles.scss'
+
+const CategoryList = ({ categories }) => {
+  return (
+    <div className="categories-container">
+      {categories.map((category) => {
+        return <CategoryItem category={category} key={category.id} />;
+      })}
+    </div>
+  );
+};
+
+export default CategoryList;
