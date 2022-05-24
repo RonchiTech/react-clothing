1c958ab7d955b19e72ad741bbc758c8042426a6c
ronchitech <ronchibusiness@gmail.com>
2022-05-24 18:09:04 +0800

    transform and add styled-components in input components

--- src/components/inputs/input.components.jsx
+++ src/components/inputs/input.components.jsx
@@ -1,18 +1,14 @@
-import './input.styles.scss';
+import { FormInputLabel, Group, FormInput } from './input.styles';
 const Input = ({ label, ...otherProps }) => {
   return (
-    <div className="group">
-      <input className="form-input" {...otherProps} />
+    <Group>
+      <FormInput {...otherProps} />
       {label && (
-        <label
-          className={`${
-            otherProps.value.length ? 'shrink' : ''
-          } form-input-label`}
-        >
+        <FormInputLabel shrink={otherProps.value.length}>
           {label}
-        </label>
+        </FormInputLabel>
       )}
-    </div>
+    </Group>
   );
 };
 
